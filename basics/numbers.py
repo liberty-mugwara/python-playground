5e544a48f8ae9e107aa4b82ae7f2667b9aedeb6e
Liberty Mugwara <libertymugwara@live.com>
2023-01-03 10:09:22 +0200

    feat: played with built in number types

---
+++ basics/numbers.py
@@ -0,0 +1,8 @@
+# built in types
+var_int = 10000000
+var_float = 4500.89
+var_complex = 58 + 9j
+
+print(f"type of var_int is: {type(var_int)}\n")
+print(f"type of var_float is: {type(var_float)}\n")
+print(f"type of var_complex is: {type(var_complex)}\n")
