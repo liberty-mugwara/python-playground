17aab3e0f8433a02d1a2b4f3f56396b2ce16aaad
Liberty Mugwara <libertymugwara@live.com>
2023-01-04 18:53:33 +0200

    feat: played with math operators

--- numbers/numbers.py
+++ numbers/numbers.py
@@ -6,3 +6,106 @@ var_complex = 58 + 9j
 print(f"type of var_int is: {type(var_int)}\n")
 print(f"type of var_float is: {type(var_float)}\n")
 print(f"type of var_complex is: {type(var_complex)}\n")
+
+# big number separators
+big_number = 1_000_000_000
+print(f"type of big_number is: {type(big_number)} and value is:{big_number}\n")
+
+# exponential values are always floats
+e_notation_number = 1e6
+print(
+    f"type of e_notation_number is: {type(e_notation_number)} and value is:{e_notation_number}\n"
+)
+# converting numbers
+print("======> converting numbers <======\n")
+var_int = 10
+var_float = 9.7
+
+var_int_as_float = float(var_int)
+print(f"Converting {var_int} to float: New value is equal to {var_int_as_float}\n")
+
+var_float_as_int = int(var_float)
+print(
+    f"Converting {var_float} to interger: New value is equal to {var_float_as_int}\n NB: It rounds down.\n"
+)
+
+# arithmetic operations
+var_int = 10
+var_float = 4.5
+print("======> arithmetic operations <======\n")
+print("### Adition ###\n")
+print(
+    f"{type(var_int).__name__} + {type(var_int).__name__} = {type(var_int + var_int).__name__}"
+)
+print(
+    f"{type(var_int).__name__} + {type(var_float).__name__} = {type(var_int + var_float).__name__}"
+)
+print(
+    f"{type(var_float).__name__} + {type(var_float).__name__} = {type(var_float + var_float).__name__}"
+)
+
+print("\n### Subtraction ###\n")
+print(
+    f"{type(var_int).__name__} - {type(var_int).__name__} = {type(var_int - var_int).__name__}"
+)
+print(
+    f"{type(var_int).__name__} - {type(var_float).__name__} = {type(var_int - var_float).__name__}"
+)
+print(
+    f"{type(var_float).__name__} - {type(var_float).__name__} = {type(var_float - var_float).__name__}"
+)
+
+print("\n### Multiplication ###\n")
+print(
+    f"{type(var_int).__name__} * {type(var_int).__name__} = {type(var_int * var_int).__name__}"
+)
+print(
+    f"{type(var_int).__name__} * {type(var_float).__name__} = {type(var_int * var_float).__name__}"
+)
+print(
+    f"{type(var_float).__name__} * {type(var_float).__name__} = {type(var_float * var_float).__name__}"
+)
+
+print("\n### Division ###\n")
+print(
+    f"{type(var_int).__name__} / {type(var_int).__name__} = {type(var_int / var_int).__name__}"
+)
+print(
+    f"{type(var_int).__name__} / {type(var_float).__name__} = {type(var_int / var_float).__name__}"
+)
+print(
+    f"{type(var_float).__name__} / {type(var_float).__name__} = {type(var_float / var_float).__name__}"
+)
+
+print("\n### Floor Division ###\n")
+print(
+    f"{type(var_int).__name__} // {type(var_int).__name__} = {type(var_int // var_int).__name__}"
+)
+print(
+    f"{type(var_int).__name__} // {type(var_float).__name__} = {type(var_int // var_float).__name__}"
+)
+print(
+    f"{type(var_float).__name__} // {type(var_float).__name__} = {type(var_float // var_float).__name__}"
+)
+
+print("\n### Exponential Operator ###\n")
+print(
+    f"{type(var_int).__name__} ** {type(var_int).__name__} = {type(var_int ** var_int).__name__}"
+)
+print(
+    f"{type(var_int).__name__} ** {type(var_float).__name__} = {type(var_int ** var_float).__name__}"
+)
+print(
+    f"{type(var_float).__name__} ** {type(var_float).__name__} = {type(var_float ** var_float).__name__}"
+)
+
+print("\n### Modulas Operator ###\n")
+print(
+    f"{type(var_int).__name__} % {type(var_int).__name__} = {type(var_int % var_int).__name__}"
+)
+print(
+    f"{type(var_int).__name__} % {type(var_float).__name__} = {type(var_int % var_float).__name__}"
+)
+print(
+    f"{type(var_float).__name__} % {type(var_float).__name__} = {type(var_float % var_float).__name__}"
+)
