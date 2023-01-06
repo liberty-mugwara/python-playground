70ffabe6eb8f400ba7bd8188758dbd4fde259044
Liberty Mugwara <libertymugwara@live.com>
2023-01-06 21:18:48 +0200

    feat: played with math fabs, factorial, and gcd

--- my-numbers/math.py
+++ my-numbers/math.py
@@ -35,3 +35,8 @@ f_number = math.floor(my_number)
 print(f"Number is equal to {my_number}")
 print(f"The result of ceiling is: {c_number}, type is: {type(c_number).__name__}")
 print(f"The result of floor is: {f_number}, type is: {type(f_number).__name__}")
+
+print("\n===> absolute value, gcd, factorial <===\n")
+print(f"The absolute value of -10 is : {math.fabs(-10)}")
+print(f"The factorial of 5 is : {math.factorial(5)}")
+print(f"The greatest common devisor of 1156 and 2264 is : {math.gcd(1156,2264)}")
