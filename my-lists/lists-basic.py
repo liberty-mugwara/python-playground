6db7ebc2a2f28fb6a4c90c90249d46bf37617260
Liberty Mugwara <libertymugwara@live.com>
2023-01-08 07:40:53 +0200

    play: list size/length

--- my-lists/lists-basic.py
+++ my-lists/lists-basic.py
@@ -1,7 +1,10 @@
 # Initializing a list
 my_list = ["apples", "bananas", "milk"]
-my_list2 = list((1, "lberty", True))
+my_list2 = list((1, "lberty", True, False, []))
 
 print("\n>>> Lists Basics <<<\n")
 print(f"my_list = {my_list}, type = {type(my_list).__name__}")
 print(f"my_list2 = {my_list2}, type = {type(my_list2).__name__}")
+
+print("\n>>> Lists size/length <<<\n")
+print(f"size of my_list is {len(my_list)}, size of my_list2 is {len(my_list2)}")
