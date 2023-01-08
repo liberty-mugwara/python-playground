d522c03bfb03bb176ae53f4ee12f399317205470
Liberty Mugwara <libertymugwara@live.com>
2023-01-08 12:12:27 +0200

    play: list slices

--- my-lists/lists-intermediate.py
+++ my-lists/lists-intermediate.py
@@ -4,3 +4,11 @@ list_2 = [4, 5, 6]
 print("\n>>> List operations <<<\n")
 print(f"list_1 = {list_1}, list_2 = {list_2}, list_1 + list_2 = {list_1 + list_2}")
 print(f"list_1 = {list_1}, list_1 * 3 = {list_1 * 3}")
+
+print("\n>>> List Slices <<<\n")
+list_3 = list_1 + list_2
+print(f"list_3 = {list_3}")
+print(f"list_3 all items: {list_3[:]}")
+print(f"list_3 first 3 items are {list_3[:3]}")
+print(f"list_3[4:] = {list_3[4:]}")
+print(f"list_3[1:] = {list_3[1:]}")
