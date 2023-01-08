17378a975f7d9abf7427c6e649390cd1ec3be302
Liberty Mugwara <libertymugwara@live.com>
2023-01-08 08:13:05 +0200

    play: the * operator on lists

--- my-lists/lists-intermediate.py
+++ my-lists/lists-intermediate.py
@@ -3,3 +3,4 @@ list_2 = [4, 5, 6]
 
 print("\n>>> List operations <<<\n")
 print(f"list_1 = {list_1}, list_2 = {list_2}, list_1 + list_2 = {list_1 + list_2}")
+print(f"list_1 = {list_1}, list_1 * 3 = {list_1 * 3}")
