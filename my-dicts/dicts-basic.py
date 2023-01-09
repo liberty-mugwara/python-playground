9aca995d4604c168e82de35cd4de0880f9bc5724
Liberty Mugwara <libertymugwara@live.com>
2023-01-09 20:17:23 +0200

    style: renamed util folder to utils

--- my-dicts/dicts-basic.py
+++ my-dicts/dicts-basic.py
@@ -4,5 +4,5 @@ my_dict = {"id": 1, "name": "Liberty", "age": 26}
 my_dict2 = dict([("id", 1), ("name", "Liberty"), ("age", 26)])
 
 print("\n>>> Dict Basics <<<\n")
-print(f"my_dict = {my_dict}, type = {type(my_dict).__name__}")
-print(f"my_dict2 = {my_dict2}, type = {type(my_dict2).__name__}")
+print(f"my_dict = {prettyPrint(my_dict)}, type = {type(my_dict).__name__}")
+print(f"my_dict2 = {prettyPrint(my_dict2)}, type = {type(my_dict2).__name__}")
